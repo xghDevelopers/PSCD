a3c6e5f220ef017f0e3c3408940d700f99e34c35
Thalles Batista <thallesmweb@gmail.com>
2013-10-05 22:21:19 -0300

    commit inicial

---
+++ PSCD-ejb/src/main/java/com/xghDevelopers/NewSessionBean.java
@@ -0,0 +1,20 @@
+package com.xghDevelopers;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author martin
+ */
+@Stateless
+@LocalBean
+public class NewSessionBean {
+
+    public void businessMethod() {
+    }
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+
+}
---
+++ deploy.sh
@@ -0,0 +1,3 @@
+mvn clean	package
+cp PSCD-ear/target/PSCD-ear-1.0.ear $DOMAIN_GLASSFISH/autodeploy
+
